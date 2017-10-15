ffee50624c1bb42e99195b4783c0400907466879
VP Nagraj <vpnagraj@virginia.edu>
2017-10-15 15:17:14 -0400

    hello() now features cowsay integration

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,7 @@
 #' Say hello
 #'
-#' @param name specify what you would like the program to call you;
+#' @param name specify what you would like the program to call you; defaults to user
+#' @param animal this argument allows you to choose an animal to accompany your message; animals to choose from are
 #'
 #' @export
 #'
@@ -8,10 +9,16 @@
 #' hello()
 #' hello("hal")
 
-hello <- function(name = "user") {
+hello <- function(name = "user", animal = NULL) {
 
   msg <- paste0("hello ", name, " ...")
 
-  cat(msg)
+  if(is.null(animal)) {
+
+    animal <- sample(names(cowsay::animals), 1)
+
+  }
+
+  cowsay::say(msg, by = animal)
 
 }
